653c323577101c4068e0a6fe4900a6ab8b8b82d7
Cordavi <michaelamundsen@gmail.com>
2016-06-06 23:42:17 -0400

    Logic with nested arrays

--- DeployOnDay1Redux/FISTicTacToeGame.m
+++ DeployOnDay1Redux/FISTicTacToeGame.m
@@ -23,6 +23,10 @@
     if(self) {
         // Do initialization of your game here, inside this if statement.
         // Leave the rest of this method alone :)
+        
+        //Clears game board
+        [self resetBoard];
+        
 
     }
 
@@ -31,36 +35,76 @@
 
 -(void)resetBoard
 {
+    [self.board removeAllObjects];
+    self.board = [@[[@[@"", @"", @""] mutableCopy],
+                    [@[@"", @"", @""] mutableCopy],
+                    [@[@"", @"", @""] mutableCopy]] mutableCopy];
 
 }
 
 -(NSString *)playerAtColumn:(NSUInteger)column row:(NSUInteger)row
 {
-    return @"";
+    return self.board[column][row];
 }
 
 -(BOOL)canPlayAtColumn:(NSUInteger)column row:(NSUInteger)row
 {
+    if (!([self.board[column][row]  isEqualToString:@""])) {
+        return NO;
+    }
+    
     return YES;
 }
 
 -(void)playXAtColumn:(NSUInteger)column row:(NSUInteger)row
 {
-
+    [self.board[column] replaceObjectAtIndex:row withObject:@"X"];
 }
 
 -(void)playOAtColumn:(NSUInteger)column row:(NSUInteger)row
 {
-
+    [self.board[column] replaceObjectAtIndex:row withObject:@"O"];
 }
 
--(NSString *)winningPlayer
-{
+-(NSString *)winningPlayer {
+    
+    for (NSUInteger i = 0; i < [self.board count]; i++) {
+        
+        //vert win
+        if (![self.board[i][1] isEqualToString:@""] && [self.board[i][1] isEqualToString:self.board[i][0]] && [self.board[i][1] isEqualToString:self.board[i][2]]) {
+            return self.board[i][1] ;
+        }
+        
+        //hori win
+        if (![self.board[0][i] isEqualToString:@""] && [self.board[0][i] isEqualToString:self.board[1][i]] && [self.board[0][i] isEqualToString:self.board[2][i]]) {
+            return self.board[0][i];
+        }
+    }
+    
+    //diag win
+    if ([self.board[0][0] isEqualToString:self.board[1][1]] && [self.board[0][0] isEqualToString:self.board[2][2]] ) {
+        return self.board[0][0];
+    }
+    
+    if ([self.board[0][2] isEqualToString:self.board[1][1]] && [self.board[0][2] isEqualToString:self.board[2][0]] ) {
+        return self.board[0][2];
+    }
+    
+
     return @"";
+
 }
 
 -(BOOL)isADraw
 {
+    for (NSUInteger i = 0; i < [self.board count]; i++) {
+        
+        for (NSUInteger j = 0; i < [self.board count]; j++) {
+            if (![self.board[i][j] isEqualToString:@"@"]) {
+                return YES;
+            }
+        }
+    }
     return NO;
 }
 
